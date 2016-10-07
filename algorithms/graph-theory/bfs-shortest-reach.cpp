d1d5d3621a98545df053c7d43e9462483abbefd1
cizl <david.lozic@fer.hr>
2016-10-08 00:24:52 +0200

    Solved BFS: Shortest Reach

--- algorithms/graph-theory/bfs-shortest-reach.cpp
+++ algorithms/graph-theory/bfs-shortest-reach.cpp
@@ -2,6 +2,8 @@
 #include <queue>
 #include <vector>
 
+#define DIST 6
+
 int main() {
 
 	int q; // number of queryies
@@ -15,44 +17,48 @@ int main() {
 
 		std::vector<std::vector<int> > am;
 		std::vector<int> v;
-		am.insert(am.begin(), v, n);
+		am.insert(am.begin(), n, v);
 
 		for (int j = 0; j < m; j++) {
 			int a, b;
 			std::cin >> a >> b;
-			am[a].push_back(b);
-			am[b].push_back(a);
+			am[a - 1].push_back(b - 1);
+			am[b - 1].push_back(a - 1);
 		}
 
 		int s; // starting node
 		std::cin >> s;
 
 		std::vector<int> dist;
-		dist.insert(dist.begin(), -1, n);
+		dist.insert(dist.begin(), n, -1);
 
 		int d = 0;
 
-		std::queue<int> q;
-		q.push(s);
+		std::queue<std::pair<int, int> > q;
+		q.push(std::make_pair(s - 1, 0));
 
 		while (!q.empty()) {
-			int node = q.front();
+			std::pair<int, int> p = q.front();
 			q.pop();
+
+			int node = p.first;
+			int d = p.second;
+
+			if (dist[node] != -1) continue;
 
 			dist[node] = d;
 
 			for (std::vector<int>::iterator it = am[node].begin(); 
-					it != am[node].end; it++) {
-
-				
+					it != am[node].end(); it++) {
+				q.push(std::make_pair(*it, d + DIST));
 			}
-
-		
-
 		}
-		
-		
 
+		for (int j = 0; j < n; j++) {
+			if (dist[j] == 0) continue;
+			std::cout << dist[j] << " ";
+		}
+		std::cout << std::endl;
 	}
 
 	return 0;
