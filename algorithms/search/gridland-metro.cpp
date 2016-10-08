173510e381c3ee8163dd77b111fd8577a7d34618
cizl <david.lozic@fer.hr>
2016-10-09 01:38:13 +0200

    Solved Gridland Metro

--- algorithms/search/gridland-metro.cpp
+++ algorithms/search/gridland-metro.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <algorithm>
+
+long long int free(std::vector<std::pair<int, int>> &tracks) {
+
+	std::sort(tracks.begin(), tracks.end());
+
+	auto track = tracks.begin();
+
+	int c1 = track->first, c2 = track->second;
+
+	long long int out = 0;
+	track++;
+
+	while (track != tracks.end()) {
+		if (track->first <= c2) {
+			c2 = (c2 < track->second) ? track->second : c2;
+		} else {
+			out += (c2 - c1 + 1);
+			c1 = track->first;
+			c2 = track->second;
+		}
+		track++;
+	}
+	
+	out += (c2 - c1 + 1);
+
+	return out;
+}
 
 int main() {
 
@@ -8,73 +37,21 @@ int main() {
 
 	std::cin >> n >> m >> k;
 
-	std::vector<std::vector<std::pair<int, int> > > tracks;
-	std::vector<std::pair<int, int> > v;
-	tracks.insert(tracks.begin(), n, v);
+	std::map<int, std::vector<std::pair<int, int>>> tracks;
 
 	for (int i = 0; i < k; i++) {
-
+		
 		int r, c1, c2;
 		std::cin >> r >> c1 >> c2;
-		r--; c1--; c2--;
-
-		std::pair<int, int> track = std::make_pair(c1, c2);
-		std::cout << "track: " << track.first << " " << track.second << std::endl;
-
-		// Binary search
-		int left = 0, right = tracks[r].size() - 1;
-
-		while (left <= right) {
-			int mid = left + (right - left) / 2;
-
-			if (tracks[r][left].first < track.first) {
-				left = mid + 1;
-			}
-			else if (tracks[r][right].first > track.first) {
-				right = mid - 1;
-			}
-			else {
-				break;
-			}
-			
-		}
-
-		std::cout << ".............. " << std::endl;
-		for (auto a : tracks[r]) {
-			std::cout << a.first << " " << a.second << " | ";
-		}
-		std::cout << std::endl;
-		std::cout << track.first << " " << track.second << " -> " << left << std::endl;
-		std::cout << ".............. " << std::endl;
-
-		if (tracks[r].empty()) {
-			tracks[r].push_back(track);
-		}
-		else if (track.first == tracks[r][left].first) {
-std::cout << "    | " << tracks[r][left].first << " " << tracks[r][left].second
-	<< " <> " << track.first << " " << track.second << "   -> " << left<< std::endl;
-
-			if (track.second > tracks[r][left].second) {
-				tracks[r][left].second = track.second;
-			}
-		}
-		else if (track.first <= tracks[r][left - 1].second + 1) {
-			if (track.second > tracks[r][left - 1].second) {
-				tracks[r][left - 1].second = track.second;
-			}
-		}
-		else {
-			tracks[r].insert(tracks[r].begin() + left, track);
-		}
+		tracks[r].push_back(std::make_pair(c1, c2));
 	}
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < tracks[i].size(); j++) {
-			std::cout << "(" << tracks[i][j].first << ", " << tracks[i][j].second
-				<< ") - ";
-		}
-		std::cout << std::endl;
+	long long int result = (long long)n * m;
+	for (auto &it : tracks) {
+		result -= free(it.second);
 	}
+
+	std::cout << result << std::endl;
 
 	return 0;
 }
